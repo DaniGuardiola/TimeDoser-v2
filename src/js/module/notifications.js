a269b5a604842f5e9c94393402d4bd56aeca3c53
Dani Guardiola <contacto@daniguardiola.me>
2015-08-31 14:57:46 +0200

    Fixed notification stuck on notif. center

--- src/js/module/notifications.js
+++ src/js/module/notifications.js
@@ -8,8 +8,11 @@ API.notifications = (function() {
             title: chrome.i18n.getMessage("msg3"),
             message: chrome.i18n.getMessage("msg4"),
             iconUrl: "/meta/icon-128.png"
-            // iconUrl: "/img/work.png"
+                // iconUrl: "/img/work.png"
         });
+        setTimeout(function() {
+            clear("work");
+        }, 10000);
     }
 
     // Break preset
@@ -19,13 +22,20 @@ API.notifications = (function() {
             title: chrome.i18n.getMessage("msg5"),
             message: chrome.i18n.getMessage("msg6"),
             iconUrl: "/meta/icon-128.png"
-            // iconUrl: "/img/break.png"
+                // iconUrl: "/img/break.png"
         });
+        setTimeout(function() {
+            clear("break");
+        }, 10000);
     }
 
     function create(id, opt) {
         chrome.notifications.clear(id);
         chrome.notifications.create(id, opt);
+    }
+
+    function clear(id) {
+        chrome.notifications.clear(id);
     }
 
     return {
